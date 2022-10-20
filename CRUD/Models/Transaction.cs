cf8a6f81018400fec204dd29d1e35bc951cfb923
ling <nqlcoder@gmail.com>
2022-10-20 15:18:25 +0700

    add crud asp.net

---
+++ CRUD/Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage ="This field is required")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 character")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 character")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(12, ErrorMessage = "Maximum 11 character")]
+        public string SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+        public DateTime Date { get; set; }
+    }
+}
